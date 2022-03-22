0770da3e36bd1b78cda942d190505f7008e749ae
scrapix <jaroboo@yandex.ru>
2022-03-22 21:35:11 +0100

    renamed multitran-cli to multitran_cli

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="multitran-cli",
-    version="0.1.0",
+    name="multitran_cli",
+    version="0.1.1",
     description="Unofficial multitran.com command line interface",
     long_description=open('README.md').read(),
     author='scrapix',
-    url="https://github.com/scrapix/multitran-cli",
+    url="https://github.com/scrapix/multitran_cli",
     packages=find_packages(),
     license="License",
-    scripts=["scripts/multitran-cli.py"],
+    scripts=["scripts/multitran_cli.py"],
     install_requires=["bs4", "requests", "colorama"]
 )
